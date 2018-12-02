2cf4c8ca00f358bf64559e9b4c80cd9207f39ce8
AAmanzi <alnick.amanzi@gmail.com>
2018-12-02 18:56:27 +0100

    Task 2: Choosing offered time

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,12 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Task 1
+
+            /*
             int priceOfChocolate, priceOfRose, priceOfBattery;
             priceOfChocolate = int.Parse(Console.ReadLine());
             priceOfRose = int.Parse(Console.ReadLine());
             priceOfBattery = int.Parse(Console.ReadLine());
 
             int priceTotal = priceOfChocolate + priceOfRose + 3 * priceOfBattery;
+
 
             if (priceTotal <= 10)
                 Console.WriteLine("Novcanica od 10");
@@ -21,6 +25,22 @@ namespace ConsoleApp1
                 Console.WriteLine("Novcanica od 50");
             else if (priceTotal <= 100)
                 Console.WriteLine("Novcanica od 100");
+            */
+
+            //Task 2
+
+            var busyStart = int.Parse(Console.ReadLine());
+            var busyEnd = int.Parse(Console.ReadLine());
+            var offeredTime1 = int.Parse(Console.ReadLine());
+            var offeredTime2 = int.Parse(Console.ReadLine());
+
+            
+            if (offeredTime1 < busyStart || offeredTime1 > busyEnd)
+                Console.WriteLine(offeredTime1);
+            else if (offeredTime2 < busyStart || offeredTime2 > busyEnd)
+                Console.WriteLine(offeredTime2);
+            else
+                Console.WriteLine("Nijedan termin ne odgovara");
         }
     }
 }
